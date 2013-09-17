94902d4967f2509975007f10fa25d2115081121a
Neeraj <bedi.neeraj@gmail.com>
2013-09-17 20:58:05 +0000

    mainclass updated

--- FunnyQuotes/src/com/neerajb/main/MainClass.java
+++ FunnyQuotes/src/com/neerajb/main/MainClass.java
@@ -2,11 +2,12 @@ package com.neerajb.main;
 
 import com.neerajb.quotes.Funny;
 import com.neerajb.quotes.QuoteGenerator;
+import com.neerajb.quotes.Sad;
 
 public class MainClass {
 
 	public static void main(String[] args) {
-		QuoteGenerator obj = new Funny();
+		QuoteGenerator obj = new Sad();
 		System.out.println(obj.getQuote());
 	}
 
