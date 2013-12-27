277c7bb0286fd259dbcf6bb6a8ab7542a67b3711
Neeraj <n.bedi@elsevier.com>
2013-12-27 08:26:52 -0500

    [NB] - yellow class added

---
+++ SecondProject/src/com/neerajb/colors/Yellow.java
@@ -0,0 +1,19 @@
+package com.neerajb.colors;
+
+public class Yellow {
+	private String colorName;
+	
+	public String getColor(){
+		return "The color represented by this class is: "+this.colorName;
+	}
+
+	public String getColorName() {
+		return colorName;
+	}
+
+	public void setColorName(String colorName) {
+		this.colorName = colorName;
+	}
+	
+	
+}
--- SecondProject/src/com/neerajb/main/MainClass.java
+++ SecondProject/src/com/neerajb/main/MainClass.java
@@ -1,10 +1,14 @@
 package com.neerajb.main;
 
+import com.neerajb.colors.Yellow;
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("MainClass.main()");
+		Yellow obj = new Yellow();
+		obj.setColorName("Yellow");
+		System.out.println(obj.getColor());
 	}
 
 }
