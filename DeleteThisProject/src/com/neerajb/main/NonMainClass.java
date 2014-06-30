06b820dc0fe28ce3e002b7081df3d5c545ad56c5
bedi.neeraj <Neeraj_Bedi@cable.comcast.com>
2014-06-30 13:49:50 -0400

    test commit

---
+++ DeleteThisProject/src/com/neerajb/main/NonMainClass.java
@@ -0,0 +1,13 @@
+package com.neerajb.main;
+
+import com.neerajb.quotes.QuoteGenerator;
+import com.neerajb.quotes.Sad;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		QuoteGenerator obj = new Sad();
+		System.out.println(obj.getQuote());
+	}
+
+}
